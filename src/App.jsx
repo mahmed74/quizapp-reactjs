22f412f6c8cfd12e0a7f8bccdc21b1560263748a
mahmed74 <ahmedsheikh0704@gmail.com>
2021-05-03 07:42:23 +0500

    quiz app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import quizData from "./components/quizData"
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [currentQuestion , setCurrentQuestion] = useState(0);
+  const [selected , setSelected] = useState();
+  const [options , setOptions] = useState([]);
+  const [currQues, setcurrQues] = useState(0);
+
+
 
   const [showScore , setShowScore] = useState(false);
 
-  const [toGreen , setToGreen] = useState(false);
 
   const [score , setScore] = useState(0);
 
-  const handleNextBtnClick = () =>{
-    const nextQuestion = currentQuestion + 1;
-    if(nextQuestion<quizData.length){
-      setCurrentQuestion(nextQuestion);
-    }else{
-      setShowScore(true);
+
+  useEffect(()=>{
+    setOptions(handleShuffle([quizData[currQues]?.correct_answer , ...quizData[currQues]?.incorrect_answers ]))
+},[])
+
+  const handleShuffle = (optionss) =>{
+      return optionss.sort(()=> Math.random() - 0.5);
+  }
+
+  // setOptions(handleShuffle([quizData[currQues]?.correct_answer , ...quizData[currQues]?.incorrect_answers ]))
+
+  
+ 
+
+
+
+
+  const handleSelect = (AnswerOption) =>{
+   if(selected ===  AnswerOption && selected=== quizData[currQues]?.correct_answer){
+      return 'select';
+   }else if(selected === AnswerOption && selected !== quizData[currQues]?.correct_answer){
+     return 'wrong';
+   }
+   else if(AnswerOption === quizData[currQues]?.correct_answer){
+     return 'select';
+   }
+  }
+
+  const handleCheck = (AnswerOption) =>{
+    setSelected(AnswerOption);
+    if(AnswerOption === quizData[currQues]?.correct_answer){
+      setScore(score+1);
     }
   }
 
-  const handleCheckAnswer = (isCorrect ,AnswerOption) =>{
-      if(isCorrect === true){
-        alert("this correct");
-        setToGreen(true);
-        setScore(score+1);
-        console.log(AnswerOption);
-      }
+  const handleNext = () =>{
+    if(currQues > quizData.length){
+      setShowScore(true);
+    }
+    else if(selected){
+      setcurrQues(currQues+1);
+      setSelected();
+    }else{
+      alert('please select an opt first')
+    }
   }
+
+
 
   return (
     <>
@@ -43,21 +77,21 @@ function App() {
            {showScore ? (<div className="quizBody">your score is {score}</div>) : 
             <div className="quizBody">
             <div id="questions">
-                <h2>{quizData[currentQuestion].question}</h2>
+                <h2>{quizData[currQues].question}</h2>
                 <ul className="optionGroup">
                     {/* <li className="option">option 1</li>
                     <li className="option">option 2</li>
                     <li className="option">option 3</li>
                     <li className="option">option 4</li> */}
                     {
-                      quizData[currentQuestion].options.map((AnswerOption)=> <li className="option" onClick={()=>handleCheckAnswer(AnswerOption.isCorrect , AnswerOption)}>{AnswerOption.option}</li> )
+                      options.map((AnswerOption)=> <button className={`option ${ selected && handleSelect(AnswerOption)} `} disabled={selected} onClick={()=>{handleCheck(AnswerOption)}} key={AnswerOption} >{AnswerOption}</button> )
                     }
                 </ul>
             </div>
             <div className="seesoltuion">
-              <p>{quizData[currentQuestion].solution}</p>
+              {/* <p>{quizData[currentQuestion].solution}</p> */}
             </div>
-            <div> <button className="btnNext" onClick={handleNextBtnClick}>Next</button></div>
+            <div> <button className="btnNext" onClick={handleNext}>Next</button></div>
         </div>}
     </div>
     </div>
--- src/components/quizData.jsx
+++ src/components/quizData.jsx
@@ -1,34 +1,148 @@
-const quizData = [
+// const quizData = [
+//     {
+//         question:"this is question1",
+//         options:[
+//             {option:"option1" , isCorrect:false},
+//             {option:"option2" , isCorrect:false},
+//             {option:"option3 correct" , isCorrect:true},
+//             {option:"option4" , isCorrect:false},
+//         ],
+//         solution:"this is the solution of the question 1",
+//     },
+//     {
+//         question:"this is question2",
+//         options:[
+//             {option:"option1" , isCorrect:false},
+//             {option:"option2 correct" , isCorrect:true},
+//             {option:"option3" , isCorrect:false},
+//             {option:"option4" , isCorrect:false},
+//         ],
+//         solution:"this is the solution of the question 2",
+//     },
+//     {
+//         question:"this is question3",
+//         options:[
+//             {option:"option1 correct" , isCorrect:true},
+//             {option:"option2" , isCorrect:false},
+//             {option:"option3" , isCorrect:false},
+//             {option:"option4" , isCorrect:false},
+//         ],
+//         solution:"this is the solution of the question 3",
+//     }
+// ]
+
+
+const quizData= [
     {
-        question:"this is question1",
-        options:[
-            {option:"option1" , isCorrect:false},
-            {option:"option2" , isCorrect:false},
-            {option:"option3 correct" , isCorrect:true},
-            {option:"option4" , isCorrect:false},
-        ],
-        solution:"this is the solution of the question 1",
+    
+    
+    question: "Which best selling toy of 1983 caused hysteria, resulting in riots breaking out in stores?",
+    correct_answer: "Cabbage Patch Kids",
+    incorrect_answers: [
+    "Transformers",
+    "Care Bears",
+    "Rubik&rsquo;s Cube"
+    ]
     },
     {
-        question:"this is question2",
-        options:[
-            {option:"option1" , isCorrect:false},
-            {option:"option2 correct" , isCorrect:true},
-            {option:"option3" , isCorrect:false},
-            {option:"option4" , isCorrect:false},
-        ],
-        solution:"this is the solution of the question 2",
+    
+    
+    question: "In past times, what would a gentleman keep in his fob pocket?",
+    correct_answer: "Watch",
+    incorrect_answers: [
+    "Money",
+    "Keys",
+    "Notebook"
+    ]
     },
     {
-        question:"this is question3",
-        options:[
-            {option:"option1 correct" , isCorrect:true},
-            {option:"option2" , isCorrect:false},
-            {option:"option3" , isCorrect:false},
-            {option:"option4" , isCorrect:false},
-        ],
-        solution:"this is the solution of the question 3",
+    
+    
+    question: "Which American president appears on a one dollar bill?",
+    correct_answer: "George Washington",
+    incorrect_answers: [
+    "Thomas Jefferson",
+    "Abraham Lincoln",
+    "Benjamin Franklin"
+    ]
+    },
+    {
+    
+    
+    question: "What is the nickname of the US state of California?",
+    correct_answer: "Golden State",
+    incorrect_answers: [
+    "Sunshine State",
+    "Bay State",
+    "Treasure State"
+    ]
+    },
+    {
+    
+    
+    question: "Which candy is NOT made by Mars?",
+    correct_answer: "Almond Joy",
+    incorrect_answers: [
+    "M&amp;M&#039;s",
+    "Twix",
+    "Snickers"
+    ]
+    },
+    {
+    
+    
+    question: "When someone is inexperienced they are said to be what color?",
+    correct_answer: "Green",
+    incorrect_answers: [
+    "Red",
+    "Blue",
+    "Yellow"
+    ]
+    },
+    {
+    
+    
+    question: "If you are caught &quot;Goldbricking&quot;, what are you doing wrong?",
+    correct_answer: "Slacking",
+    incorrect_answers: [
+    "Smoking",
+    "Stealing",
+    "Cheating"
+    ]
+    },
+    {
+    
+    
+    question: "What is the closest planet to our solar system&#039;s sun?",
+    correct_answer: "Mercury",
+    incorrect_answers: [
+    "Mars",
+    "Jupiter",
+    "Earth"
+    ]
+    },
+    {
+    
+    
+    question: "The Canadian $1 coin is colloquially known as a what?",
+    correct_answer: "Loonie",
+    incorrect_answers: [
+    "Boolie",
+    "Foolie",
+    "Moodie"
+    ]
+    },
+    {
+    
+    
+    question: "What is the French word for &quot;fish&quot;?",
+    correct_answer: "poisson",
+    incorrect_answers: [
+    "fiche",
+    "escargot",
+    "mer"
+    ]
     }
-]
+    ]
 
 export default quizData;
